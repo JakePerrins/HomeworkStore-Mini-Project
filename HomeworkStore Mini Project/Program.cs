d2d9468090cce72846785cfb0fea4af60c35fb26
Jake Perrins <AQ232838@aquinas.ac.uk>
2023-11-07 10:20:25 +0000

    Add project files.

---
+++ HomeworkStore Mini Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkStore_Mini_Project
+{
+    internal class Program
+    {
+
+        struct 
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
